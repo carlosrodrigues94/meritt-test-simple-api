ce2ada005044bb6be9ee91c85533a3b0b33fb0a8
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-01-28 21:35:59 -0300

    route students implemented

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express()
 app.get('/proofs',  (req, res) =>  {
   res.json(db.proofs)
 })
+
+app.get('/students',  (req, res) =>  {
+  res.json(db.students)
+})
+ 
  
 app.listen(3334, () => {
   console.info("Server running in port 3334")
