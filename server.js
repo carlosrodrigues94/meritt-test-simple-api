9110b8307fb4ef0c1eeeb024f4983e141e846e92
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-01-28 21:52:30 -0300

    favicon.ico

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/proofs',  (req, res) =>  {
 app.get('/students',  (req, res) =>  {
   res.json(db.students)
 })
+
+app.get('/favicon.ico', (req,res)=>{
+  res.send('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHbds3bgHkpbqknB1UdiqNtjOi7yY4cjtfmQ&usqp=CAU')
+} )
  
  
 app.listen(3334, () => {
